2679d29075ef15157823e544e1355fd45efafb72
Rahul <rahulr4121996@gmail.com>
2020-03-05 09:15:35 +0100

    first commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/rahul/ompt_course_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/rahul/ompt_course_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/rahul/ompt_course_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/rahul/ompt_course_ws/src:$ROS_PACKAGE_PATH"
